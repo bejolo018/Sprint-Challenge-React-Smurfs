e119c74387207106290c037557c9b243e4257a42
unknown <bejolo018@gmail.com>
2019-05-17 11:20:13 -0500

    index.js bug fix

--- village/src/index.js
+++ village/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+import App from './App';
 
 import './index.css';
 
